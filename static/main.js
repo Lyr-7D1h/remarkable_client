39eba484dc7134436442fd2027c05bfedc7d5f47
Lyr <lyr-7d1h@pm.me>
2023-03-02 12:24:56 +0100

    wip

--- static/main.js
+++ static/main.js
@@ -3,17 +3,18 @@ const filesDom = document.getElementById("files");
 
 const devices = {};
 // setInterval(() => {
-invoke("devices").then((d) => {
-  console.log(devices);
-  for (const device of d) {
-    devices[device.mac] = device;
-  }
-  render();
-});
+// invoke("devices").then((d) => {
+//   console.log(devices);
+//   for (const device of d) {
+//     devices[device.mac] = device;
+//   }
+//   render();
+// });
 // }, 1000);
 
 function render() {
   if (devices.length == 0) return;
+  console.log(devices);
 
   document.getElementById("loading_block").style.display = "none";
 
