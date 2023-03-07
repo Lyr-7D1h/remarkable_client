f568816d72081579b60cac10b9dae7028977bb3d
Lyr <lyr-7d1h@pm.me>
2023-03-07 18:00:30 +0100

    added modal

--- static/main.js
+++ static/main.js
@@ -14,13 +14,14 @@ invoke("scan").then((entries) => {
 // }, 1000);
 
 const deviceButton = (device) => `
-<button type="button" onclick="connect('${device.mac}')" style="padding-right: 15px;">
+<button type="button" class="device" onclick="connect('${device.mac}')" style="padding-right: 15px;">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64zM176 432h96c8.8 0 16 7.2 16 16s-7.2 16-16 16H176c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg>
   ${device.ip}
 </button>`;
 
-function addDevice(e) {
-  console.log(e);
+function addDevice() {
+  // TODO get form data
+  // const device =
   invoke("add_device", { mac: device.mac, device })
     .then(() => {
       closeModal();
@@ -30,9 +31,13 @@ function addDevice(e) {
 }
 
 /// Connect to a device
-function connect(device) {
+function connect(mac) {
+  const device = scan[mac];
+  console.log(device);
   if (device.known === false) {
-    modal(`<form onsubmit="addDevice()">
+    modal(`<form action="#" onsubmit="addDevice()">
+  <label for='nickname'>Device name:</label><br>
+  <input required type='text' id='nickname' placeholder="My Remarkable" name='nickname'><br>
   <label for='username'>Username:</label><br>
   <input type='text' id='username' name='username' value='root'><br>
   <label for='password'>Password:</label><br>
@@ -72,3 +77,12 @@ function modal(html) {
     if (event.target == modal) closeModal();
   };
 }
+// modal(`<form onsubmit="addDevice()">
+//   <label for='nickname'>Device name:</label><br>
+//   <input required type='text' id='nickname' placeholder="My Remarkable" name='nickname'><br>
+//   <label for='username'>Username:</label><br>
+//   <input required type='text' id='username' placeholder="username" name='username' value='root'><br>
+//   <label for='password'>Password:</label><br>
+//   <input required type='password' id='password' placeholder='password' name='password' value=''><br><br>
+//   <input type='submit' value='Connect'>
+// </form>`);
